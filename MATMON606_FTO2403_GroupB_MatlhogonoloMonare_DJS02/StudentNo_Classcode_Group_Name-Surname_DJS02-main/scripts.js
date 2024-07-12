82b51250bbd789722a059c847ae07d2e0b22b425
Cedrick Monare <162999186+CedrickMonare@users.noreply.github.com>
2024-07-12 14:03:43 +0200

    Added functions to validate values and and convert them to integers

--- MATMON606_FTO2403_GroupB_MatlhogonoloMonare_DJS02/StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
+++ MATMON606_FTO2403_GroupB_MatlhogonoloMonare_DJS02/StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
@@ -1,9 +1,30 @@
+// Form and result elements
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
+// Initial message to be displayed
+result.innerText = "No calculation performed";
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  
+  // Form entries retrieved
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
-});
+
+ try {
+    // Validation for missing values
+    if (!dividend || !divider) {
+      result.innerText = "Operation not performed. Both values are required in inputs. Please Try again.";
+      return;
+    }
+
+    // Function to convert values to integers
+    const dividendNumber = parseInt(dividend, 10);
+    const dividerNumber = parseInt(divider, 10);
+
+    // Function to check if the values are numbers
+    if (isNaN(dividendNumber) || isNaN(dividerNumber)) {
+      throw new Error("Invalid input: not a number");
+    }
